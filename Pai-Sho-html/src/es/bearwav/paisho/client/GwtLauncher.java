239b4da6f2f46adb61d7c3e91481e7024a5b27a9
Joel Auterson <joel.auterson@googlemail.com>
2014-01-15 16:36:28 +0000

    Added LibGDX generated project.

---
+++ Pai-Sho-html/src/es/bearwav/paisho/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package es.bearwav.paisho.client;
+
+import es.bearwav.paisho.GdxGame;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new GdxGame();
+	}
+}
